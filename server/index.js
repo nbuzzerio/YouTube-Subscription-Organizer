d5cc50b13288d4692b97fe6d6996de2039478080
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2020-08-17 22:23:19 -0400

    switched to server side google authentication from client side

--- server/index.js
+++ server/index.js
@@ -1,43 +1,93 @@
 const express = require('express');
-const axios = require('axios');
 require('dotenv').config();
-
-const { google } = require('googleapis');
-
 const app = express();
 
-app.use(express.static('./client/dist'))
+const CONFIG = require('../config/youtube.config.js')
 
-app.get('/search', (req, res) => {
-  google.youtube('v3').search.list({
-    key: process.env.YouTubeAPIKey,
-    part: 'snippet',
-    q: 'saveafox'
-  }).then( (response) => {
-    // console.log(response);
-    res.send(response.data);
-  }).catch((err) => {
-    console.log(err)
-    res.send('There was an error while searching')
-  })
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+
+
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+
+app.use(cookieParser())
+// app.use(express.static('./client/dist'))
+
+//Send Login Screen
+app.get('/', (req, res) => {
+  const oauth2client = new OAuth2(
+    CONFIG.oauth2Credentials.client_id,
+    CONFIG.oauth2Credentials.client_secret,
+    CONFIG.oauth2Credentials.redirect_uris[0],
+    )
+
+    const loginLink = oauth2client.generateAuthUrl({
+      access_type: 'offline',
+      scope: CONFIG.oauth2Credentials.scopes
+    })
+
+    return res.send(`
+    <h1>WELCOME TO YOUTUBE SUBSCRIPTION ORGANIZER</h1>
+    <h5>please log in</h5>
+    <a href=${loginLink}>Login With Google</a>
+    `)
 })
 
-app.get('/subscriptions', (req, res) => {
-  google.youtube('v3').subscriptions.list({
-    key: process.env.YouTubeAPIKey,
-    part: 'snippet',
-    q: 'saveafox'
-  }).then( (response) => {
-    console.log(response);
-    res.send(response.data);
-  }).catch((err) => {
-    console.log(err)
-    res.send('There was an error while searching')
+//Handle authorization step
+app.get('/OAuth', (req, res) => {
+  const oauth2client = new OAuth2(
+    CONFIG.oauth2Credentials.client_id,
+    CONFIG.oauth2Credentials.client_secret,
+    CONFIG.oauth2Credentials.redirect_uris[0],
+    )
+  
+  if (req.query.error){
+    //if user rejects permissions
+    return res.redirect('/');
+  } else {
+    oauth2client.getToken(req.query.code, (err, token) => {
+      if (err) {
+        return res.redirect('/')
+      }
+      res.cookie('jwt', jwt.sign(token, CONFIG.JWTsecret));
+
+      return res.redirect('/subscription_organizer');
+    })
+  }
+
+  app.get('/subscription_organizer', (req, res) => {
+    
+    if (!req.cookies.jwt) {
+      return res.redirect('/')
+    }
+
+    const oauth2client = new OAuth2(
+      CONFIG.oauth2Credentials.client_id,
+      CONFIG.oauth2Credentials.client_secret,
+      CONFIG.oauth2Credentials.redirect_uris[0],
+      )
+
+    oauth2client.credentials = jwt.verify(req.cookies.jwt, CONFIG.JWTsecret);
+
+    const youtube = google.youtube('v3');
+    
+    youtube.subscriptions.list({
+      auth: oauth2client,
+      mine: true,
+      part: 'snippet, contentDetails',
+      maxResults:50
+    })
+    .then( (response) => {
+      console.log(response)
+
+      return res.send({subscriptions: response.data.items})
+    })
+
   })
+  
 })
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`YTSubOrganizer app listening at http://localhost:${port}`);
+app.listen(CONFIG.port, () => {
+  console.log(`YouTube-Subscription-Organizer app listening at ${CONFIG.baseUrl}`);
 })
