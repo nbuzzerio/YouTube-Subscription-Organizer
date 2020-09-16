bda688b6dd05894beb09c0a6b85aeffd379863bc
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2020-09-16 00:55:59 -0400

    paginated subs and added buttons for pages and added some styles

--- client/src/components/SubCarousel.jsx
+++ client/src/components/SubCarousel.jsx
@@ -1,11 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import SubFilter from './SubFilter.jsx';
 import Sub from './Sub.jsx';
 
 function SubCarousel({ subs }) {
     const [filter, setFilter] = useState('');
+    const [page, setPage] = useState(1)
 
     let filteredList = [];
+    let lastPage = Math.ceil(subs.length / 34);
+
+    const handleClick = (e) => {
+        
+        if (e.target.innerText === '>') {
+            if (page < lastPage) {
+                setPage(page+1);
+            }
+        } else if (e.target.innerText === '<') {
+            if (page > 1) {
+                setPage(page-1);
+            }
+        }
+    };
 
     if (subs.length > 0) {
         filteredList = subs.filter(sub => sub.Channel_Name.toLowerCase().includes(filter.toLowerCase()) || sub.Channel_Description.toLowerCase().includes(filter.toLowerCase()));
@@ -13,10 +28,18 @@ function SubCarousel({ subs }) {
     
     let listsubs = filteredList.map((sub, i) => <Sub sub={sub} key={i} />);
 
+    let currentListSubs = listsubs.filter((sub, i) => i < (34 * page) && i >= (34 * page) - 34);
+
     return (
         <div className='subCarousel'>
-            <div className='subsWrapper'>
-                {listsubs}
+            <div className='subsWrapper'>               
+                <div className='pageBtn' id='back' onClick={handleClick}>
+                    <p className='pageBtnTxt'>{'<'}</p>
+                </div>
+                <div className='pageBtn' id='next' onClick={handleClick}>
+                    <p className='pageBtnTxt'>{'>'}</p>
+                </div>
+                {currentListSubs}
             </div>
             <SubFilter setFilter={setFilter} />
         </div>
